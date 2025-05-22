954f9c9e045546606d9996f1110ab756a659d06a
Sanarielsen <sanarielsen.dev@gmail.com>
2025-05-22 14:31:59 -0300

    feat(GHB-10): create a panel to receive the post selected
    Create a panel to receive the post clicked in a home page, the post
    will be generated by Markdown of Issue and here I will put this code
    to show the context of current article.
    
    Resolves: GHB-10

---
+++ src/features/Post/components/PostContext.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@features/Post/styles/PostContextContainer";
+
+export function PostContext() {
+  return (
+    <Container>
+      <p> Aqui vai ficar o conteúdo da ISSUE selecionada </p>
+    </Container>
+  )
+}
--- src/features/Post/page/Post.tsx
+++ src/features/Post/page/Post.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@features/Post/styles/PostContainer";
+import { PostContext } from "@features/Post/components/PostContext";
 import { PostHeader } from "@features/Post/components/PostHeader";
 
 export function Post() {
   return (
     <Container>
       <PostHeader />
+      <PostContext />
     </Container>
   )
 }
