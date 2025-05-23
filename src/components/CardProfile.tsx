45c1e1e138ac38767ab6de85f0f3d9d17beeeeeb
Sanarielsen <sanarielsen.dev@gmail.com>
2025-05-23 15:38:00 -0300

    feat(GHB-11): create a context to use Github API in profile
    Create a context to consume data provided by Github API in the profile
    card in home page. Necessary to use octokit and useQuery to manage
    these information can be used.
    
    I created a new path to put all calls referenced to the internal API or
    external like Github API, the folder named of API and default path too.
    
    Resolves: GHB-11

--- src/components/CardProfile.tsx
+++ src/components/CardProfile.tsx
@@ -1,5 +1,5 @@
 import { CardDescription, CardImage, CardRedirect, Container, DescriptionIcons, IconGroup } from "@globalStyles/CardProfileContainer";
-import imgPerfilExemplo from "@globalAssets/images/ExempleAvatar.png";
+import { useProfileGithub } from "@globalQueries/getProfileGithub";
 import logoCompany from "@globalAssets/images/CompanyLogo.svg"
 import logoFollow from "@globalAssets/images/FollowLogo.svg"
 import logoGithub from "@globalAssets/images/GithubLogo.svg"
@@ -7,27 +7,44 @@ import iconeRedirect from "@globalAssets/images/RedirectIcone.svg"
 
 export function CardProfile() {
 
+  const { isPending, isError, data: profile, error } = useProfileGithub();
+
+  //FEATURE: Create a loading component, error component and empty data component
+  if (isPending) 
+    return (
+      <Container>
+        <p>Loading...</p>
+      </Container>
+    )
+  if (isError)
+    return (
+      <Container>
+        <p>Error: {error.message}</p>
+      </Container>
+    )     
+  if (!profile)
+    return (
+      <Container>
+        <p>No data</p>  
+      </Container>
+    )
+
   return (
     <Container>
       <CardImage>
-        <img src={imgPerfilExemplo} alt="Foto de Perfil" />
+        <img src={profile.imageProfile} alt="Foto de Perfil" />
       </CardImage>
-      <CardRedirect href="https://github.com/Sanarielsen" target="_blank">
+      <CardRedirect href={`https://github.com/${profile.username}`} target="_blank">
         Github<img src={iconeRedirect} />
       </CardRedirect>
       <CardDescription>        
-        <h3>Cameron Williamson</h3>
-
-        <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. 
-        Eu viverra massa quam dignissim aenean malesuada suscipit. 
-        Nunc, volutpat pulvinar vel mass.</p>
-
+        <h3>{profile.name}</h3>
+        <p>{profile.description}</p>
         <DescriptionIcons>
-            <IconGroup><img src={logoGithub} />cameronwll</IconGroup>
-            <IconGroup><img src={logoCompany} />sanarielsen</IconGroup>
-            <IconGroup><img src={logoFollow} />32 seguidores</IconGroup>            
-        </DescriptionIcons>
-        
+            <IconGroup><img src={logoGithub} />{profile.username}</IconGroup>
+            <IconGroup><img src={logoCompany} />{profile.company}</IconGroup>
+            <IconGroup><img src={logoFollow} />{profile.countFollowers} seguidores</IconGroup>            
+        </DescriptionIcons>        
       </CardDescription>
     </Container>
   )
